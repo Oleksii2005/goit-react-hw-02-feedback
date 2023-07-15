60fbe8b3df7f0977002407e49fd1bc9ec64e5dd7
Oleksii2005 <122476976+Oleksii2005@users.noreply.github.com>
2023-07-15 23:52:44 +0200

    adding first task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,72 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import css from './Feedback/Feedback.module.css';
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+  goodBtnHandleClick = () => {
+    this.setState(prevState => {
+      return {
+        good: prevState.good + 1,
+      };
+    });
+  };
+  neutralBtnHandleClick = () => {
+    this.setState(prevState => {
+      return {
+        neutral: prevState.neutral + 1,
+      };
+    });
+  };
+  badBtnHandleClick = () => {
+    this.setState(prevState => {
+      return {
+        bad: prevState.bad + 1,
+      };
+    });
+  };
+  render() {
+    return (
+      <div className="container">
+        <div className="feedback">
+          <h2>Please leave feedback</h2>
+          <div className="feedback_btns">
+            <button
+              type="button"
+              className={`${css.btn} ${css.btn_good}`}
+              onClick={this.goodBtnHandleClick}
+            >
+              Good
+            </button>
+            <button
+              type="button"
+              className={`${css.btn} ${css.btn_neutral}`}
+              onClick={this.neutralBtnHandleClick}
+            >
+              Neutral
+            </button>
+            <button
+              type="button"
+              className={`${css.btn} ${css.btn_bad}`}
+              onClick={this.badBtnHandleClick}
+            >
+              Bad
+            </button>
+          </div>
+        </div>
+        <div className="statistics">
+          <div className="statistics_options">
+            <p>Good:{this.state.good}</p>
+            <p>Neutral:{this.state.neutral}</p>
+            <p>Bad:{this.state.bad}</p>
+            <p>Total:</p>
+            <p>Positive feedback:</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+export default App;
